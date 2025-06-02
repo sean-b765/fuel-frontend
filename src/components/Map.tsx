761026ac869397ba614257a97d2b66eae576e8dd
Sean Boaden <sean.b0153@gmail.com>
2025-06-02 16:15:05 +0800

    Changes to MAP

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,36 +6,45 @@ import {
   Grid,
   Typography,
 } from "@mui/material"
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl"
-import { Map as MapboxGL } from 'mapbox-gl'
+import ReactMapboxGL from "react-mapbox-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { useStore } from "../state/state"
-import React, { Ref, useEffect, useRef } from "react"
+import React, { Ref, useEffect, useRef, useState } from "react"
+import Pin from "./Pin"
+import mapboxGl, { Map as MapboxGLMap, Layer, Marker } from "mapbox-gl"
+import UserLocationMarker from "./UserLocationMarker"
+import SelectedStationMarker from "./SelectedStationMarker"
+import disabledPin from "../assets/disabled-pin.png"
+import StationMarkers from "./StationMarkers"
 
-const Mapbox = ReactMapboxGl({
-  accessToken:
-    "pk.eyJ1Ijoic2VhbmIwMTUzIiwiYSI6ImNtYjYzZHI2MjBoOGkyanFzeGdhcm0xYXMifQ.VDYysO0qDb5wBACX7zWH2A",
-})
+mapboxGl.accessToken = process.env.REACT_APP_MAPBOX_KEY
 
-// TODO this will load each time selectedStation changes, eating up my API key usage quota
-//  It should load ONCE only. Any subsequent changes to selectedStation should animate the map to the next station
 const Map = ({}) => {
+  const mapRef = useRef<MapboxGLMap | null>(null)
+  const mapContainerRef = useRef<HTMLDivElement | null>(null)
   const selectedStation = useStore((state) => state.selectedStation)
-  const userLocation = useStore((state) => state.userLocation)
-  const mapRef = useRef<any>(null)
 
+  // Initialise the map
   useEffect(() => {
-    if (mapRef.current == null || selectedStation == undefined) return
-    console.log(mapRef.current, selectedStation)
+    if (mapRef.current || !mapContainerRef.current) return // initialized already, or no container available
 
-    // mapRef.current.flyTo({
-    //   center: [Number(selectedStation.Latitude), Number(selectedStation.Longitude)]
-    // })
-  }, [selectedStation])
+    const map = new MapboxGLMap({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/dark-v11",
+      center: [115.86256114388702, -31.950664055777565],
+      zoom: 12,
+    })
 
-  if (selectedStation == undefined) return <></>
+    map.on("style.load", () => {
+      mapRef.current = map
 
-  if (userLocation == undefined) return <></>
+      // Register images
+      mapRef.current.loadImage(disabledPin, (err, img) => {
+        if (mapRef.current == null || err) return
+        mapRef.current.addImage("disabled-pin", img as ImageBitmap)
+      })
+    })
+  }, [])
 
   return (
     <Card variant="outlined">
@@ -51,65 +60,69 @@ const Map = ({}) => {
             </Typography>
           </Grid>
           <Grid mt={2} size={12}>
-            <Grid size={12} minHeight={300} sx={{ position: "relative" }}>
-              <Mapbox
-                ref={mapRef}
-                style="mapbox://styles/mapbox/streets-v9"
-                containerStyle={{
+            <Grid size={12} height={300} sx={{ position: "relative" }}>
+              <div
+                ref={mapContainerRef}
+                style={{
                   width: "100%",
                   height: "100%",
-                  position: "absolute"
                 }}
-                // TODO every time the selected updates, it centers to here
-                center={[userLocation.lng, userLocation.lat]}
-              ></Mapbox>
+              >
+                <UserLocationMarker map={mapRef.current} />
+                <SelectedStationMarker map={mapRef.current} />
+                <StationMarkers map={mapRef.current} />
+              </div>
             </Grid>
             {/* Price, distance */}
-            <Grid size={12} my={2}>
-              <Grid>
-                <Chip
-                  size="small"
-                  label={`$${selectedStation.Price}`}
-                  sx={{ marginRight: "8px" }}
-                />
-                <Chip
-                  size="small"
-                  label={`${
-                    Math.round(selectedStation.DistanceTo * 100) / 100
-                  }km`}
-                />
+            {selectedStation && (
+              <Grid size={12} my={2}>
+                <Grid>
+                  <Chip
+                    size="small"
+                    label={`$${selectedStation.Price}`}
+                    sx={{ marginRight: "8px" }}
+                  />
+                  <Chip
+                    size="small"
+                    label={`${
+                      Math.round(selectedStation.DistanceTo * 100) / 100
+                    }km`}
+                  />
+                </Grid>
               </Grid>
-            </Grid>
+            )}
             {/* Details */}
-            <Grid size={12} mb={2}>
-              {selectedStation.Address && (
-                <Typography variant="body2">
-                  {selectedStation.Address}
-                </Typography>
-              )}
-              {selectedStation.Location && (
-                <Typography variant="body1">
-                  {selectedStation.Location}
-                </Typography>
-              )}
-              {selectedStation.Phone && (
-                <Typography variant="overline">
-                  {selectedStation.Phone}
-                </Typography>
-              )}
-              {(selectedStation.JourneyDistance ||
-                selectedStation.JourneyTime) && <Divider sx={{ my: 1 }} />}
-              {selectedStation.JourneyTime && (
-                <Typography variant="body2">
-                  {selectedStation.JourneyTime}
-                </Typography>
-              )}
-              {selectedStation.JourneyDistance && (
-                <Typography variant="body2">
-                  {selectedStation.JourneyDistance}
-                </Typography>
-              )}
-            </Grid>
+            {selectedStation && (
+              <Grid size={12} mb={2}>
+                {selectedStation.Address && (
+                  <Typography variant="body2">
+                    {selectedStation.Address}
+                  </Typography>
+                )}
+                {selectedStation.Location && (
+                  <Typography variant="body1">
+                    {selectedStation.Location}
+                  </Typography>
+                )}
+                {selectedStation.Phone && (
+                  <Typography variant="overline">
+                    {selectedStation.Phone}
+                  </Typography>
+                )}
+                {(selectedStation.JourneyDistance ||
+                  selectedStation.JourneyTime) && <Divider sx={{ my: 1 }} />}
+                {selectedStation.JourneyTime && (
+                  <Typography variant="body2">
+                    {selectedStation.JourneyTime}
+                  </Typography>
+                )}
+                {selectedStation.JourneyDistance && (
+                  <Typography variant="body2">
+                    {selectedStation.JourneyDistance}
+                  </Typography>
+                )}
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </CardContent>
